a862122566c73b99fc7622a0da19fcf0e6146302
Ketan <145323193+ketan073@users.noreply.github.com>
2026-02-08 17:25:10 +0530

    changed db to neondb

--- backend/config.py
+++ backend/config.py
@@ -5,12 +5,13 @@ class Config:
     DATABASE_TYPE = 'postgres'
     
     # Postgres
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    DB_NAME = os.getenv('DB_NAME', 'cms_db')
-    DB_USER = os.getenv('DB_USER', 'shreerajkalbande') # Current user
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '') # Empty for local trust/ident
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', '5432')
+    # Default to hosted Neon DB for demo purposes
+    DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://neondb_owner:npg_2uC1fxpocyXV@ep-green-recipe-a1n8o5be-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
+    # DB_NAME = os.getenv('DB_NAME', 'cms_db')
+    # DB_USER = os.getenv('DB_USER', 'shreerajkalbande') # Current user
+    # DB_PASSWORD = os.getenv('DB_PASSWORD', '') # Empty for local trust/ident
+    # DB_HOST = os.getenv('DB_HOST', 'localhost')
+    # DB_PORT = os.getenv('DB_PORT', '5432')
     
     # Security
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_secret_key')
